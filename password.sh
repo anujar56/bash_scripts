143ed857eb7a3e41c648e4ef666c4eb571b7f4da
Ubuntu <ubuntu@ip-172-31-40-114.ec2.internal>
2025-02-11 10:15:34 +0000

    Anuj

---
+++ password.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+pass() {
+
+        local password=$1
+        local min_l=8
+
+        if [ ${#password} -gt $min_l ] && [[ $password =~ [a-z] ]] && [[ $password =~ [A-Z] ]] && [[ $password =~ [0-9] ]]
+        then
+		echo ""
+                echo "Password is of correct length and is alphanumeric"
+                return 0
+        else
+                echo "Try another password"
+                return 1
+        fi
+
+
+}
+
+
+show_help() {
+
+	echo "Usage: $0 [options]"
+	echo " "
+	echo "Options"
+	echo "-e : Enter the password"
+	echo "-h : help"
+	exit 0
+
+
+}
+
+
+while getopts "eh" options; do
+	case $options in
+		e)
+			read -p "Enter the password: " psd
+			pass "${psd}"
+			;;
+
+		h)
+			show_help
+			;;
+
+		?) 
+			echo "Invalid Options us: $0 -h"
+			exit 1
+			;;
+	esac
+done
+
+
+
+#read -p "Enter the password: " psd
+#pass "${psd}"
