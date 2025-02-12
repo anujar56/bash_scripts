961b9308049e3cd9916ac4b74f428e4fdebdbca0
Ubuntu <ubuntu@ip-172-31-90-81.ec2.internal>
2025-02-12 12:02:09 +0000

    log analysis file

---
+++ log_ay.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+logfile=~/log_file.log
+
+show_help() {
+
+	echo "Usage: ./log_analyzer.sh [-h] [-i] [log_file_path]"
+	echo 
+	echo "Analyze system logs to identify and summarize error messages."
+	echo
+	echo "Options:"
+	echo "-h Display this help message."
+	echo "-i Interactive mode."
+	echo "log_file_path Specify the path to the log file. Default is /var/log/syslog."
+}
+
+display() {
+	
+	echo "Select the type of log analysis to perform:"
+	echo "1. Count Errors"
+	echo "2. List Errors"
+	echo "3. Search Errors"
+	read -p "Enter your choice: " choice
+
+	case $choice in
+		1)
+			local count=$(cat $logfile | grep -i error | wc -l)
+			echo "Total number of errors: ${count}"
+			exit 0
+			;;
+		2)
+			echo "List of unique error messages and their frequencies:"
+			cat log_file.log | grep error | awk -F ":" '{print "error: " $NF}' | uniq -u
+			;;
+		3)
+			echo "Searching for errors containing the keyword 'error':"
+			cat $logfile | grep -i error
+			;;
+		?)
+			show_help
+			exit 1
+			;;
+
+	esac
+
+}
+
+if [[ $# -eq 0 ]]; then
+	show_help
+	exit 1
+fi
+
+
+
+while getopts "hi" options; do
+	case $options in
+		h)
+			show_help
+			exit 1
+			;;
+		i)
+			display
+			;;
+		?)
+			show_help
+			exit 1
+			;;
+	esac
+done
+	
